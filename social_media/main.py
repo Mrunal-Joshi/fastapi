0e53fd3e45724f83576547934585b904fe262e1c
Mrunal-Joshi <mrunaljoshiofficial@gmail.com>
2024-11-29 13:16:46 +0530

    added Response model

--- social_media/main.py
+++ social_media/main.py
@@ -4,9 +4,9 @@ apis for social media
 
 from random import randint
 from fastapi import FastAPI, status, Response, Depends
-
+from typing import List
 from social_media.constants import posts
-from social_media.schemas import CreatePost, CreateUser
+from social_media import schemas
 from social_media.utilities import find_post
 from . import models
 from .database import engine, get_db, SessionLocal
@@ -18,21 +18,21 @@ app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
 ## API'S
-@app.get("/")
+@app.get("/posts", response_model=List[schemas.Post])
 def getposts(db : Session = Depends(get_db)):
     data = db.query(models.Post).all()
-    return {"data" : data}
+    return data
 
 
-@app.get("/posts/{path_id}")
+@app.get("/posts/{path_id}", response_model=schemas.Post)
 def get_post(path_id: int, db : Session = Depends(get_db)):
     post = db.query(models.Post).filter_by(id=path_id).one_or_none()
     #post = find_post(posts, path_id)
-    return {"data" : post}
+    return post
 
 
-@app.post("/posts", status_code = status.HTTP_201_CREATED)
-def create_posts(post: CreatePost, db : Session = Depends(get_db)):
+@app.post("/posts", status_code = status.HTTP_201_CREATED, response_model=schemas.Post)
+def create_posts(post: schemas.CreatePost, db : Session = Depends(get_db)):
     # new_post = post.dict()
     # new_post["id"] = randint(0, 10000000)
     # posts.append(new_post)
@@ -40,11 +40,11 @@ def create_posts(post: CreatePost, db : Session = Depends(get_db)):
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
-    return {"message": "Post created", "post": new_post}
+    return new_post
 
 
-@app.put("/posts/{path_id}")
-def update_post(path_id: int, post_to_update: CreatePost, db : Session = Depends(get_db)):
+@app.put("/posts/{path_id}", response_model=schemas.Post)
+def update_post(path_id: int, post_to_update: schemas.CreatePost, db : Session = Depends(get_db)):
     # post = find_post(posts, path_id)
 
     # post_to_update["id"] = path_id
@@ -56,7 +56,7 @@ def update_post(path_id: int, post_to_update: CreatePost, db : Session = Depends
         raise HTTPException(status_code = status.HTTP_204_NO_CONTENT, detail = "Does not exist")
     post.update(post_to_update.dict(), synchronize_session=False)
     db.commit()
-    return {"message": "Post Updated", "post": post_to_update.dict()}
+    return post_to_update.dict()
 
 
 @app.delete("/posts/{path_id}", status_code = status.HTTP_204_NO_CONTENT)
@@ -74,11 +74,11 @@ def delete_post(path_id: int, db : Session = Depends(get_db)):
 
 
 #USER APIS
-@app.post("/users", status_code=status.HTTP_201_CREATED)
-def create_user(user: CreateUser, db: Session = Depends(get_db)):
+@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.DisplayUser)
+def create_user(user: schemas.CreateUser, db: Session = Depends(get_db)):
     new_user = models.User(**user.dict())
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
 
-    return {"message": "USER created", "post": new_user}
+    return new_user
--- social_media/schemas.py
+++ social_media/schemas.py
@@ -3,16 +3,34 @@ Data model for posts
 """
 
 from pydantic import BaseModel, EmailStr
+from datetime import datetime
 
-
-class Posts(BaseModel):
+class PostBase(BaseModel):
     title: str
     content: str
     published: bool = True
 
-class CreatePost(Posts):
+class CreatePost(PostBase):
     pass
+
+
+#Response Model
+class Post(PostBase):
+    id : int
+    created_at : datetime
+
+    # it will tell the pydantic model to read data even if it's not a dict but any obj with attributes
+    class Config:
+        from_attributes = True
 
 class CreateUser(BaseModel):
     email : EmailStr
     password : str
+
+class DisplayUser(BaseModel):
+    id : int
+    email : EmailStr
+
+    # Response will be in sqlalchemy model, hence convert it to pydantic model
+    class Config:
+        from_attributes = True
