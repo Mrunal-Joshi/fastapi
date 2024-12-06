7d15fd79f36624e2f67de6d5d0b0c4c8e1cc972e
Mrunal-Joshi <mrunaljoshiofficial@gmail.com>
2024-12-06 17:56:09 +0530

    updated relationship retrival

--- social_media/models.py
+++ social_media/models.py
@@ -1,7 +1,9 @@
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from .database import Base
+
 
 
 class Post(Base):
@@ -13,6 +15,10 @@ class Post(Base):
     published = Column(Boolean, server_default="true", nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
     owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False) #refers table name not class name
+
+
+    # SQLAlchemy automatically imports  the relationship fields
+    owner = relationship("User")
 
 class User(Base):
     __tablename__ = "users"
--- social_media/schemas.py
+++ social_media/schemas.py
@@ -6,26 +6,6 @@ from pydantic import BaseModel, EmailStr
 from datetime import datetime
 from typing import Optional
 
-
-class PostBase(BaseModel):
-    title: str
-    content: str
-    published: bool = True
-    # user_id: int -- whoever is looged in, take that user id
-
-class CreatePost(PostBase):
-    pass
-
-
-#Response Model
-class Post(PostBase):
-    id : int
-    created_at : datetime
-    owner_id : int
-    # it will tell the pydantic model to read data even if it's not a dict but any obj with attributes
-    class Config:
-        from_attributes = True
-
 class CreateUser(BaseModel):
     email : EmailStr
     password : str
@@ -49,3 +29,22 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     id : Optional[str] = None
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    # user_id: int -- whoever is looged in, take that user id
+
+class CreatePost(PostBase):
+    pass
+
+
+#Response Model
+class Post(PostBase):
+    id : int
+    created_at : datetime
+    owner : DisplayUser
+    # it will tell the pydantic model to read data even if it's not a dict but any obj with attributes
+    class Config:
+        from_attributes = True
